64356e0a5e7f48e6cabd77aa3f5863e2c82541ea
sanchalex <odarchenko@ex.ua>
2017-05-02 22:11:03 +0300

    update templates

--- flora_project/urls.py
+++ flora_project/urls.py
@@ -13,9 +13,20 @@ Including another URLconf
     1. Import the include() function: from django.conf.urls import url, include
     2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
 """
+from django.conf import settings
 from django.conf.urls import url
+from django.conf.urls.static import static
 from django.contrib import admin
+from django.views.generic import TemplateView
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
+    url(r'^$', TemplateView.as_view(template_name='main_page/main.html'), name='main_page'),
+    url(r'^forum$', TemplateView.as_view(template_name='forum/forum.html'), name='forum'),
+    url(r'^forum/1$', TemplateView.as_view(template_name='forum/forum_single_post.html'), name='forum_single'),
+    url(r'^gallery$', TemplateView.as_view(template_name='galary/gallery.html'), name='gallery')
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
