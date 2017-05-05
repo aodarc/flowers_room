f3fd8e26138bfe11689860e640f0de1851ec4bfd
sanchalex <odarchenko@ex.ua>
2017-05-05 23:40:20 +0300

    add editor and models

--- apps/gallary/urls.py
+++ apps/gallary/urls.py
@@ -1,8 +1,10 @@
 from django.conf.urls import url
 from django.views.generic import TemplateView
 
-from apps.gallary.views import GalleryView
+from apps.gallary.views import GalleryView, PhotoView
 
 urlpatterns = [
-    url(r'$', GalleryView.as_view(), name='gallery')
+    url(r'/(?P<pk>\d+)/photos$', PhotoView.as_view(), name='gallery-photo'),
+    url(r'/(?P<pk>\d+)/photos/(?P<p_pk>\d+)$', PhotoView.as_view(), name='gallery-photo-detail'),
+    url(r'$', GalleryView.as_view(), name='gallery'),
 ]
--- apps/gallary/views.py
+++ apps/gallary/views.py
@@ -1,9 +1,10 @@
 from django.shortcuts import render
 
 # Create your views here.
+from django.views import View
 from django.views.generic import TemplateView
 
-from apps.gallary.models import Album
+from apps.gallary.models import Album, Photo
 
 
 class GalleryView(TemplateView):
@@ -15,5 +16,29 @@ class GalleryView(TemplateView):
 
         context['data'] = queryset
         context['albums'] = True
+
+        return context
+
+
+class PhotoView(TemplateView):
+    template_name = 'galary/gallery.html'
+
+    def get_context_data(self, **kwargs):
+        context = {}
+        queryset = Photo.objects.filter(album_id=self.kwargs['pk'])[:12]
+
+        context['data'] = queryset
+        context['photos'] = True
+
+        return context
+
+class PhotoDetailView(TemplateView):
+    template_name = 'galary/gallery.html'
+
+    def get_context_data(self, **kwargs):
+        context = {}
+        queryset = Photo.objects.get(id=self.kwargs['pk'])
+
+        context['photo'] = queryset
 
         return context
