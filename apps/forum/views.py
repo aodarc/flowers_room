43d5272073e8b19c1b4248d09d01a8b4e58448d5
sanchalex <odarchenko@ex.ua>
2017-05-08 22:58:24 +0300

    add easy search

--- apps/forum/views.py
+++ apps/forum/views.py
@@ -1,3 +1,4 @@
+from django.db.models import Q
 from django.shortcuts import render, get_object_or_404, redirect
 
 # Create your views here.
@@ -15,9 +16,23 @@ class ForumView(FooterContextMixin, RightSideContextMixin, TemplateView):
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
 
+        search = self.request.GET.get('search')
+        if search:
+            context['posts'] = Post.objects.filter(self.make_search_query(search)).distinct()
+            return context
+
         context['posts'] = Post.objects.all()
 
         return context
+
+    def make_search_query(self, search_text):
+
+        words = (word for word in search_text.split() if word)
+        query = Q()
+        for word in words:
+            query |= Q(title__contains=word) | Q(description__contains=word) | Q(tags__name__contains=word) | Q(
+                category__name__contains=word)
+        return query
 
 
 class SinglePost(FooterContextMixin, RightSideContextMixin, TemplateView):
@@ -33,7 +48,6 @@ class SinglePost(FooterContextMixin, RightSideContextMixin, TemplateView):
 
 
 def add_comment(request, pk):
-
     if request.method == 'POST':
         post = get_object_or_404(Post, id=pk)
         form = CommentForm(request.POST)
