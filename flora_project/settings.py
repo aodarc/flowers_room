fcd09c8398fed3224c8336165250b18111ceb655
aodarc <odarchenko@ex.ua>
2018-11-29 22:28:59 +0200

    fix game

--- flora_project/settings.py
+++ flora_project/settings.py
@@ -37,7 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'ckeditor',
     'ckeditor_uploader',
-    'sass_processor',
+    # 'sass_processor',
     'apps.forum',
     'apps.advice',
     'apps.gallary',
